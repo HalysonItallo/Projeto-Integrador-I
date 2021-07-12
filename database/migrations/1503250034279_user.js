165e197cb6de82b39bc3a9bd3f4f9d7f969ca44f
HalysonItallo <halyssonpimentell@gmail.com>
2021-07-12 13:40:02 -0300

    Atualizando o git

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -1,32 +1,32 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('name', 40).notNullable()
-      table.string('full_name', 80).notNullable()
-      table.string('RG', 10).unique()
-      table.string('CPF', 15).unique()
-      table.string('registration', 20).unique()
-      table.string('road',80)
-      table.string('district',80)
-      table.string('number_house',80)
-      table.string('email', 254).notNullable().unique()
-      table.string('telephone', 20).notNullable()
-      table.string('password', 60).notNullable()
-      table.string('profile_picture', 500).notNullable()
-      table.string('type', 20).notNullable()
-      table.timestamps()
-    })
+  up() {
+    this.create("users", (table) => {
+      table.increments();
+      table.string("name", 40).notNullable();
+      table.string("full_name", 80).notNullable();
+      table.string("RG", 10).unique();
+      table.string("CPF", 15).unique();
+      table.string("registration", 20).unique();
+      table.string("road", 80);
+      table.string("district", 80);
+      table.string("number_house", 80);
+      table.string("email", 254).notNullable().unique();
+      table.string("telephone", 20).notNullable();
+      table.string("password", 60).notNullable();
+      table.string("profile_picture", 500).notNullable();
+      table.string("type", 20).notNullable();
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('users')
+  down() {
+    this.drop("users");
   }
 }
 
-module.exports = UserSchema
+module.exports = UserSchema;
